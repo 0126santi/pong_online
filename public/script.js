8e1dd0eb073d939749539bf479a9f9bc4586c7df
Santiago Rodriguez <Srodriguezl.2601@gmail.com>
2025-04-27 16:22:11 -0400

    agregando contador de iniciar partida

--- public/script.js
+++ public/script.js
@@ -10,6 +10,11 @@ let score = { p1: 0, p2: 0 }, keys = {}, gameRunning = false;
 
 const interpolationFactor = 0.2;
 
+// Variables para el contador
+let countdown = 0;
+let countdownInterval;
+let showCountdown = false;
+
 function createRoom() {
   room = document.getElementById('roomName').value;
   if (room) socket.emit('createRoom', room);
@@ -46,6 +51,20 @@ function checkGameOver() {
   }
 }
 
+function startCountdown(callback) {
+  countdown = 3;
+  showCountdown = true;
+
+  countdownInterval = setInterval(() => {
+    countdown--;
+    if (countdown === 0) {
+      clearInterval(countdownInterval);
+      showCountdown = false;
+      callback();
+    }
+  }, 1000);
+}
+
 function update() {
   if (!gameRunning) return;
 
@@ -65,19 +84,17 @@ function update() {
     if (ball.x >= 770 && ball.y >= rightPaddle.y && ball.y <= rightPaddle.y + 100) ball.vx *= -1.05;
 
     if (ball.x <= 0) {
-      socket.emit('goalScored', { roomName: room, scorer: 'p2' }); // Gol para jugador 2
-      ball = { x: 400, y: 250, vx: 5, vy: 3 };
-  }
+      socket.emit('goalScored', { roomName: room, scorer: 'p2' });
+      resetBall();
+    }
     if (ball.x >= 800) {
-      socket.emit('goalScored', { roomName: room, scorer: 'p1' }); // Gol para jugador 1
-      ball = { x: 400, y: 250, vx: -5, vy: 3 };
-  }
-  
+      socket.emit('goalScored', { roomName: room, scorer: 'p1' });
+      resetBall();
+    }
 
     socket.emit('ballUpdate', { roomName: room, ball, score });
     checkGameOver();
   } else {
-    // Interpolación hacia la posición enviada por el host
     const maxDist = 50;
     const dx = ballTarget.x - ball.x;
     const dy = ballTarget.y - ball.y;
@@ -91,13 +108,27 @@ function update() {
   }
 }
 
+function resetBall() {
+  ball = { x: 400, y: 250, vx: (Math.random() > 0.5 ? 5 : -5), vy: (Math.random() > 0.5 ? 3 : -3) };
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "white";
+
+  if (showCountdown) {
+    ctx.font = "100px sans-serif";
+    ctx.textAlign = "center";
+    ctx.fillText(countdown > 0 ? countdown : "¡YA!", canvas.width / 2, canvas.height / 2);
+    return;
+  }
+
   ctx.fillRect(leftPaddle.x, leftPaddle.y, 10, 100);
   ctx.fillRect(rightPaddle.x, rightPaddle.y, 10, 100);
   ctx.fillRect(ball.x, ball.y, 10, 10);
+
   ctx.font = "20px sans-serif";
+  ctx.textAlign = "left";
   ctx.fillText(`Jugador 1: ${score.p1}`, 100, 30);
   ctx.fillText(`Jugador 2: ${score.p2}`, 600, 30);
 }
@@ -129,8 +160,11 @@ socket.on('canStartGame', () => {
 socket.on('startGame', (initialBall) => {
   ball = initialBall;
   ballTarget = initialBall;
-  gameRunning = true;
   document.getElementById('startBtn').style.display = 'none';
+
+  startCountdown(() => {
+    gameRunning = true;
+  });
 });
 
 socket.on('opponentMove', (y) => {
@@ -144,6 +178,14 @@ socket.on('ballUpdate', ({ ball: b }) => {
 
 socket.on('scoreUpdate', (s) => {
   score = s;
+  if (isHost) {
+    resetBall();
+    ballTarget = ball;
+    gameRunning = false;
+    startCountdown(() => {
+      gameRunning = true;
+    });
+  }
 });
 
 socket.on('resetGame', (initialBall) => {
